98fd14f05b66681213ef5d4992c7b8336df99ff9
claudiaalozano <clau.lozano.perez@gmail.com>
2023-02-27 15:57:10 +0100

    archivo secuelcial

---
+++ secuencialmente_url.py
@@ -0,0 +1,13 @@
+from multiprocessing import Pool
+import random
+from time import sleep
+
+urls=["a.com", "b.com", "c.com", "d.com"]
+def scrape(url):
+    print("starting", url)
+    duration= round(random.random(), 3)
+    sleep(duration)
+    print("done", url,"time taken: " ,duration, " seconds.")
+    return url, duration
+    
+
